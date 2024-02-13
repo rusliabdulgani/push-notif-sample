9e63c89cf8b82f4488d32c4139cfac3fc17e5227
rusli abdul gani <abdul.gani@fullertonhealth.com>
2024-02-13 20:26:28 +0700

    update

--- src/onesignal.js
+++ src/onesignal.js
@@ -2,7 +2,7 @@ import OneSignal from 'react-onesignal';
 
 export default async function runOneSignal() {
   try {
-    await OneSignal.init({ appId: 'cf54b259-5bce-4024-806c-326918718874', allowLocalhostAsSecureOrigin: true});
+    await OneSignal.init({ appId: 'cf54b259-5bce-4024-806c-326918718874'});
     OneSignal.Slidedown.promptPush();
   } catch (error) {
     console.log('error one signal', error)
